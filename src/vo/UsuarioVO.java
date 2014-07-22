876e46ff32593adbcbd8882ab93d11b95390eb70
Fredericko <fredericko.xg@hotmail.com>
2014-07-22 16:48:04 -0400

    Tabelas das relações muitos para muitos agora atualizam automaticamente.
    Configuração da navegação pelo arquivo faces-config.

--- src/vo/UsuarioVO.java
+++ src/vo/UsuarioVO.java
@@ -3,12 +3,13 @@ package vo;
 import java.util.Collection;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -29,9 +30,7 @@ public class UsuarioVO {
 	private Boolean estadoLogado;
 	private Double credito;
 
-	@ManyToMany(targetEntity=JogoVO.class)
-	@JoinTable(name = "usuario_jogo", joinColumns={@JoinColumn(name="idUsuario")}, inverseJoinColumns=
-  {@JoinColumn(name="idJogo")})
+	@ManyToMany(targetEntity=JogoVO.class, fetch=FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<JogoVO> jogos;
 	
 	@ManyToOne
