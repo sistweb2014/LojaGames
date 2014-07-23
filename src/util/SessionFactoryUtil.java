201fd8e17031294975ac6f30ca9440733357f5b1
Rafael Sanzio Macedo Porto <rafael.sanzio@outlook.com>
2014-07-23 10:22:24 -0400

    Adicionado interface do login do usuário e estilização, feito por Rodrigo Rafael. Classe que implementa a encripitação da senha, criação da classe de controle do usuário, tratamento de das exceções do usuários.

--- src/util/SessionFactoryUtil.java
+++ src/util/SessionFactoryUtil.java
@@ -1,23 +1,10 @@
 package util;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import negocio.Pedido;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-
-import vo.JogoVO;
-import vo.PagamentoVO;
-import vo.PedidoVO;
-import vo.UsuarioVO;
-import vo.enumerado.TipoPagamento;
-import vo.enumerado.TipoPedido;
 
 public class SessionFactoryUtil {
 
@@ -52,44 +39,5 @@ public class SessionFactoryUtil {
 		}
 		sessionFactory = null;
 	}
-
-	public static void main(String[] args) {
-		Session s = SessionFactoryUtil.getInstance().openSession();
-		UsuarioVO vo = new UsuarioVO();
-		vo.setNome("Produto TESTE");
-		
-		JogoVO jogoVO = new JogoVO();
-		jogoVO.setNome("TESTE");
-		
-		PedidoVO pedidoVO = new PedidoVO();
-		pedidoVO.setTipoPedido(TipoPedido.COMPRA);
-		
-		PagamentoVO pagamentoVO = new PagamentoVO();
-		pagamentoVO.setPedido(pedidoVO);
-		pedidoVO.setPagamento(pagamentoVO);
-
-		List<JogoVO> listaJogo = new ArrayList<JogoVO>();
-		listaJogo.add(jogoVO);
-		
-		List<UsuarioVO> listaUsuario= new ArrayList<UsuarioVO>();
-		listaUsuario.add(vo);
-		
-		List<PedidoVO> listaPedido = new ArrayList<PedidoVO>();
-		listaPedido.add(pedidoVO);
-		
-		vo.setJogos(listaJogo);
-		vo.setPedidos(listaPedido);
-		
-		jogoVO.setUsuarios(listaUsuario);
-		
-		Transaction t = s.beginTransaction();
-		s.save(vo);
-		s.save(jogoVO);
-		s.save(pedidoVO);
-		s.save(pagamentoVO);
-		t.commit();
-		
-		System.out.println(vo.getJogos().size());
-		System.out.println(vo.getJogos().get(0).getNome());
-	}
+	
 }
