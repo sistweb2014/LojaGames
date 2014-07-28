2f903db7b81852838bdf326bd4b6d9990b31c143
Rafael Sanzio Macedo Porto <rafael.sanzio@outlook.com>
2014-07-28 10:19:24 -0400

    Inicio do ManageBean para o Rodrigo

--- src/controle/UsuarioControle.java
+++ src/controle/UsuarioControle.java
@@ -1,5 +1,57 @@
 package controle;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import negocio.Usuario;
+import vo.UsuarioVO;
+import vo.excecao.UsuarioVOException;
+
+@ManagedBean
+@SessionScoped
 public class UsuarioControle {
+
+	private Usuario usuario;
+	private UsuarioVO vo;
+	
+	public UsuarioControle() {
+		usuario = new Usuario();
+		vo = new UsuarioVO();
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public UsuarioVO getVo() {
+		return vo;
+	}
+
+	public void setVo(UsuarioVO vo) {
+		this.vo = vo;
+	}
+
+	public void cadastrarUsuario(ActionEvent event) {
+		try {
+			usuario.save(vo);
+			vo = new UsuarioVO();
+			FacesContext.getCurrentInstance().addMessage("formCadastro",
+					new FacesMessage("Usuário Cadstrado com sucesso!"));
+		} catch (UsuarioVOException e) {
+			FacesContext.getCurrentInstance().addMessage("formCadastro",
+					new FacesMessage(e.getMessage()));
+		}
+	}
+
+	public void excluirProduto() {
+		usuario.delete(vo);
+	}
 
 }
--- src/negocio/Usuario.java
+++ src/negocio/Usuario.java
@@ -1,12 +1,25 @@
 package negocio;
 
 import java.util.List;
+
 import dao.UsuarioDAO;
 import vo.UsuarioVO;
+import vo.excecao.UsuarioVOException;
 
 public class Usuario {
 
-	public void save(UsuarioVO vo) {
+	public void save(UsuarioVO vo) throws UsuarioVOException {
+		if(vo.getNome() == null && vo.getNome().isEmpty())
+			throw new UsuarioVOException(UsuarioVOException.NOMEOBRIGATORIO);
+		if(vo.getNome() == null && vo.getLogin().isEmpty())
+			throw new UsuarioVOException(UsuarioVOException.LOGINOBRIGATORIO);
+		if(vo.getEmail() == null && vo.getEmail().isEmpty())
+			throw new UsuarioVOException(UsuarioVOException.EMAILOBRIGATORIO);
+		if(vo.getSenha() == null && vo.getSenha().isEmpty())
+			throw new UsuarioVOException(UsuarioVOException.SENHAOBRIGATORIO);
+		if(vo.validarEmail(vo.getEmail()))
+			throw new UsuarioVOException(UsuarioVOException.EMAILINVALIDO);
+		
 		UsuarioDAO.getInstance().save(vo);
 	}
 	
